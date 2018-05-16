851a4d7847c825eec0e55acde15b55a4830892d2
Brett Cimbalik <bcimbali@gmail.com>
2018-05-15 23:14:16 -0500

    basic file structure started

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black Bean and Goat Cheese', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beyond Burger and Lettuce and Tomato', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Slider', false);
